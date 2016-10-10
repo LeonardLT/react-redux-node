3ec321563b5f5cbbc1098112b05f443c534726d2
luojinghui <939035243@qq.com>
2016-10-10 15:19:59 +0800

    add two webpack.config.file

---
+++ config/entries.config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by: Luojinghui/luojinghui424@gmail.com
+ * Date: 16/10/10
+ * Time: 下午12:10
+ */
+module.exports = [
+    {
+        name: 'index',
+        title: '锦囊专家'
+    },
+    {
+        name: 'admin',
+        title: '锦囊管理'
+    }
+];
