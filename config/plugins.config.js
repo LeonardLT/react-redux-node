e9868e1c87c981777dfe8cd930e1c31515b86007
luojinghui <939035243@qq.com>
2016-10-10 15:19:59 +0800

    add two webpack.config.file

---
+++ config/plugins.config.js
@@ -0,0 +1,38 @@
+/**
+ * Created by: Luojinghui/luojinghui424@gmail.com
+ * Date: 16/10/10
+ * Time: 下午12:40
+ */
+const webpack = require('webpack');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const pageUrl = require('./entries.config.js');
+
+var configPlugins = [];
+
+var occ = new webpack.optimize.OccurenceOrderPlugin();
+var hot = new webpack.HotModuleReplacementPlugin();
+var no = new webpack.HotModuleReplacementPlugin();
+var opt = new webpack.optimize.CommonsChunkPlugin({
+    name: 'commons', // 将公共模块提取，生成名为`vendors`的chunk
+    chunks: ['index','admin'], //提取哪些模块共有的部分
+    filename: '[name]/bundle.js',
+    minChunks: 4,
+});
+var extra = new ExtractTextPlugin('[name]/style.css', {allChunks: false});
+
+configPlugins = [occ, hot, no, opt, extra];
+
+pageUrl.forEach((page) => {
+    const htmlPlugin = new HtmlWebpackPlugin({
+        filename: `${page.name}/index.html`,
+        title: page.title,
+        template: 'public/index.html',
+        chunks: [page.name, 'commons'],
+        hash: true, // 为静态资源生成hash值
+    });
+    configPlugins.push(htmlPlugin);
+})
+
+module.exports = configPlugins;
