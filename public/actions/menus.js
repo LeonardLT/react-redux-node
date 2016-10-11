14adc2668d1d3198a360a77bc78e1e6e0f1fc653
luojinghui <939035243@qq.com>
2016-10-11 15:23:05 +0800

    add redux for actions

---
+++ public/actions/menus.js
@@ -0,0 +1,18 @@
+/**
+ * Created by: Luojinghui/luojinghui424@gmail.com
+ * Date: 16/10/10
+ * Time: 下午8:23
+ */
+
+import request from 'superagent'
+
+export const loadMenus = (dispatch) => {
+    request
+        .get('/api/json')
+        .end((err,res) => {
+            dispatch({
+                type: 'menusLoaded',
+                data: res.body
+            })
+        })
+}
