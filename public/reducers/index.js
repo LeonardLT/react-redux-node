4beabf872f804a92122a1a7a6e3cf31ee5367f72
luojinghui <939035243@qq.com>
2016-10-11 15:25:36 +0800

    add reducers

---
+++ public/reducers/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by: Luojinghui/luojinghui424@gmail.com
+ * Date: 16/10/10
+ * Time: 下午8:18
+ */
+import { combineReducers } from 'redux'
+import menus from './menus'
+
+const reducers = combineReducers({
+    menus
+})
+
+export default reducers;
