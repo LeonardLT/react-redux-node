4b074378b446439586c9956397e6f4f722612538
luojinghui <939035243@qq.com>
2016-10-12 15:14:56 +0800

    increase the judge to deal with the different state of the view

--- public/component/index/category.js
+++ public/component/index/category.js
@@ -6,64 +6,75 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
-const Category = ( {menus} ) => {
-    return (
-        <div className="container pos-rel mt">
-            <div className="cate-container">
-                <div className="category">
-                    <div className="category-box">
-                        <ul className="ca-ul">
-                            {
-                                menus.map((item, index) => {
-                                    const containerClass = item.content.length > 8 ? 'citem-detail citem-col-2' : 'citem-detail citem-col-1';
-                                    return (
-                                        <li className="ca-li" key={index}>
-                                            <a className="cate-a" href="">{item.item}</a>
-                                            <div className={containerClass}>
-                                                <ul className="citem-children citem-chi-col">
-                                                    {
-                                                        item.content.map((subMenu, index) => {
-                                                            if(index >= 8) {
-                                                                return;
-                                                            }
-                                                            return (
-                                                                <li className="citem-chi-list" key={index}>
-                                                                    <a href="#" className="choose">{subMenu}</a>
-                                                                </li>
-                                                            )
-                                                        })
-                                                    }
+class Category extends Component {
+    constructor(props) {
+        super(props)
+    }
 
-                                                </ul>
-                                                <ul className="citem-children citem-chi-col">
-                                                    {
-                                                        item.content.map((subMenu, index) => {
-                                                            if(index >= 8 && index < 16) {
+    render() {
+        let menuData = this.props.menus.menu;
+        //
+        return menuData ? (
+            <div className="container pos-rel mt">
+                <div className="cate-container">
+                    <div className="category">
+                        <div className="category-box">
+                            <ul className="ca-ul">
+                                {
+                                    menuData.map((item, index) => {
+                                        const containerClass = item.content.length > 8 ? 'citem-detail citem-col-2' : 'citem-detail citem-col-1';
+                                        return (
+                                            <li className="ca-li" key={index}>
+                                                <a className="cate-a" href="">{item.item}</a>
+                                                <div className={containerClass}>
+                                                    <ul className="citem-children citem-chi-col">
+                                                        {
+                                                            item.content.map((subMenu, index) => {
+                                                                if(index >= 8) {
+                                                                    return;
+                                                                }
                                                                 return (
                                                                     <li className="citem-chi-list" key={index}>
                                                                         <a href="#" className="choose">{subMenu}</a>
                                                                     </li>
                                                                 )
-                                                            }
-                                                        })
-                                                    }
+                                                            })
+                                                        }
 
-                                                </ul>
-                                            </div>
-                                        </li>
-                                    )
-                                })
-                            }
+                                                    </ul>
+                                                    <ul className="citem-children citem-chi-col">
+                                                        {
+                                                            item.content.map((subMenu, index) => {
+                                                                if(index >= 8 && index < 16) {
+                                                                    return (
+                                                                        <li className="citem-chi-list" key={index}>
+                                                                            <a href="#" className="choose">{subMenu}</a>
+                                                                        </li>
+                                                                    )
+                                                                }
+                                                            })
+                                                        }
 
-                        </ul>
+                                                    </ul>
+                                                </div>
+                                            </li>
+                                        )
+                                    })
+                                }
+
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        ) : <span></span>
+
+    }
 }
 
-const mapStateToProps = (state) => (state)
+const mapStateToProps = (state) => {
+    return state;
+}
 
 // category.propTypes = {};
 // category.defaultProps = {};
