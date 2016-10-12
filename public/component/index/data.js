c8721cb540a89610592d2f8af3c718d8d6dd9e66
luojinghui <939035243@qq.com>
2016-10-12 15:15:30 +0800

    Increase the three-eye operation to deal with the different state of the view

--- public/component/index/data.js
+++ public/component/index/data.js
@@ -10,34 +10,28 @@ const img2 = require('../../img/index/font7.png');
 const img3 = require('../../img/index/font1.png');
 const img4 = require('../../img/index/font3.png');
 
-class Data extends Component {
-    constructor(props) {
-        super(props)
-    }
+const Data = ( props ) => {
+    let numberData = props.menus.number;
 
-    render() {
-        const number = this.props.number;
-
-        return (
-            <div className="data-content tc">
-                <div className="container">
-                    <p className="data-p dp1">
-                        <span id="num1">{number[0]}</span>
-                        <img src={img1} alt="" className="data-br num1-img" />
-                            <span id="num2" className="num2">{number[1]}</span>
-                            <img src={img2} alt="" className="num2-img" />
-                    </p>
-                    <p className="data-p dp2">
-                        <span id="num3">{number[2]}</span>
-                        <img src={img3} alt="" className="data-br num3-img" />
-                            <span id="num4">{number[3]}</span>
-                            <img src={img4} alt="" className="num4-img" />
-                    </p>
-                    <div className="publish-demand btn ">发布需求</div>
-                </div>
+    return numberData ? (
+        <div className="data-content tc">
+            <div className="container">
+                <p className="data-p dp1">
+                    <span id="num1">{numberData[0]}</span>
+                    <img src={img1} alt="" className="data-br num1-img" />
+                    <span id="num2" className="num2">{numberData[1]}</span>
+                    <img src={img2} alt="" className="num2-img" />
+                </p>
+                <p className="data-p dp2">
+                    <span id="num3">{numberData[2]}</span>
+                    <img src={img3} alt="" className="data-br num3-img" />
+                    <span id="num4">{numberData[3]}</span>
+                    <img src={img4} alt="" className="num4-img" />
+                </p>
+                <div className="publish-demand btn ">发布需求</div>
             </div>
-        );
-    }
+        </div>
+    ) : <div>error...</div>
 }
 
 const mapStateToProps = (state) => (state)
