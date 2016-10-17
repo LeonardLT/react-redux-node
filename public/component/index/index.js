42a9650d60a31453d2a79242966a52e550591cea
luojinghui <939035243@qq.com>
2016-10-17 18:14:50 +0800

    alter index.js

--- public/component/index/index.js
+++ public/component/index/index.js
@@ -1,5 +1,5 @@
 // import $ from 'jquery';
-import Index from './content.js';
+import Index from './content';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
@@ -14,7 +14,8 @@ require('../../styles/index.css');
 
 const store = createStore(
     reducers,
-    applyMiddleware(thunk) //redux的原生方法,作用是将所有中间件组成一个数组,依次执行
+    applyMiddleware(thunk) //来自redux可以包装store的dispatch,redux的原生方法,作用是将所有中间件组成一个数组,依次执行
+    //applyMiddleware来自redux,可以包装store的dispatch,thunk作用是使action创建函数可以返回一个function代替一个action对象
 );
 
 store.dispatch(loadMenus);
@@ -30,5 +31,5 @@ ReactDOM.render(
 // Notice!!!
 // Following is required to make reloading happen
 if (module.hot) {
-  module.hot.accept();
+    module.hot.accept();
 }
