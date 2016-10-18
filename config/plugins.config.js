7200c8a196d7b0a0a56f0aa5bbe5ea1cccab8caa
luojinghui <939035243@qq.com>
2016-10-18 10:37:33 +0800

    add webpack config

--- config/plugins.config.js
+++ config/plugins.config.js
@@ -14,19 +14,19 @@ var configPlugins = [];
 var occ = new webpack.optimize.OccurenceOrderPlugin();
 var hot = new webpack.HotModuleReplacementPlugin();
 var no = new webpack.HotModuleReplacementPlugin();
-var opt = new webpack.optimize.CommonsChunkPlugin({
-    name: 'commons', // 将公共模块提取，生成名为`vendors`的chunk
-    chunks: ['index','admin'], //提取哪些模块共有的部分
-    filename: '[name]/bundle.js',
-    minChunks: 4,
-});
-var extra = new ExtractTextPlugin('[name]/style.css', {allChunks: false});
+// var opt = new webpack.optimize.CommonsChunkPlugin({
+//     name: 'commons', // 将公共模块提取，生成名为`vendors`的chunk
+//     chunks: ['index','admin'], //提取哪些模块共有的部分
+//     filename: '[name]/bundle.js',
+//     minChunks: 4,
+// });
+var extra = new ExtractTextPlugin('[name].css', {allChunks: false});
 
-configPlugins = [occ, hot, no, opt, extra];
+configPlugins = [occ, hot, no, extra];
 
 pageUrl.forEach((page) => {
     const htmlPlugin = new HtmlWebpackPlugin({
-        filename: `${page.name}/index.html`,
+        filename: `${page.name}.html`,
         title: page.title,
         template: 'public/index.html',
         chunks: [page.name, 'commons'],
